5ccdfb9c47de3d298ea3c2a1a600393bf77a1660
bitangaming0000-png <bitangaming0000@gmail.com>
2025-08-29 22:05:57 +0530

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -10,7 +10,12 @@ module.exports = async (req, res) => {
   ];
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(u => `<url><loc>${base}${u}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`).join('\n')}
+${urls.map(u => `
+  <url>
+    <loc>${base}${u}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>`).join('')}
 </urlset>`;
   res.setHeader('Content-Type','application/xml; charset=utf-8');
   res.status(200).send(xml);
