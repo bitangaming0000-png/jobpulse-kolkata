603065927c215c4bfe3a2fdb3aa423ed6900668c
bitangaming0000-png <bitangaming0000@gmail.com>
2025-08-29 18:18:05 +0530

    Create sitemap.js

---
+++ api/sitemap.js
@@ -0,0 +1,17 @@
+module.exports = async (req, res) => {
+  const base = 'https://jobpulse-kolkata.vercel.app';
+  const urls = [
+    '/',
+    '/pages/post.html',
+    '/pages/privacy.html',
+    '/pages/terms.html',
+    '/pages/disclaimer.html',
+    '/pages/contact.html'
+  ];
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(u => `<url><loc>${base}${u}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`).join('\n')}
+</urlset>`;
+  res.setHeader('Content-Type','application/xml; charset=utf-8');
+  res.status(200).send(xml);
+};
